974cfa45327b8c1324664dcbad0e922b98119b79
Somin Yun <somin455@naver.com>
2023-11-09 18:16:04 +0900

    week9

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,flash,redirect,url_for,session
+from database import DBhandler
+import hashlib
 import sys
 application = Flask(__name__)
+application.config["SECRET_KEY"]="helloosp"
+DB=DBhandler()
 @application.route("/")
 def hello():
     return render_template("index.html")
+@application.route("/login")
+def login():
+    return render_template("login.html")
+@application.route("/signup")
+def signup():
+    return render_template("signup.html")
+@application.route("/signup_post",methods=['POST'])
+def register_user():
+    data=request.form
+    pw=request.form['pw']
+    pw_hash=hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    if DB.insert_user(data,pw_hash):
+        return render_template("login.html")
+    else:
+        flash("user id already exist!")
+        return render_template("signup.html")
+
 
 @application.route("/list")
 def view_list():
@@ -34,6 +55,9 @@ def reg_item_submit_post():
     image_file=request.files["file"]
     image_file.save("static/images/{}".format(image_file.filename))
     data=request.form
+
+    DB.insert_item(data['name'],data,image_file.filename)
+
     return render_template("submit_item_result.html", data=data, img_path="static/images/{}".format(image_file.filename))
 
 if __name__ == "__main__":
