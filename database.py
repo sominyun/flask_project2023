272f8cc9c6c7d6777828e395ff2951516530ead7
Somin Yun <somin455@naver.com>
2023-11-22 11:46:39 +0900

    week11-1

--- database.py
+++ database.py
@@ -7,7 +7,44 @@ class DBhandler:
 
         firebase = pyrebase.initialize_app(config)
         self.db = firebase.database()
-
+    
+    # 회원가입
+    def insert_user(self,data,pw):
+        user_info={
+            "id": data['id'],
+            "pw":pw,
+            "nickname":data['nickname']
+        }
+        if self.user_duplicate_check(str(data['id'])):
+            self.db.child("user").push(user_info)
+            print(data)
+            return True
+        else:
+            return False
+        
+    # 회원가입 시 중복확인
+    def user_duplicate_check(self, id_string):
+        users = self.db.child("user").get()
+        
+        print("users###",users.val())
+        if str(users.val()) == "None": # first registration
+            return True
+        else:
+            for res in users.each():
+                value = res.val()
+                if value['id'] == id_string:
+                    return False
+            return True
+    #로그인 시 유저 찾기
+    def find_user(self, id_, pw_):
+        users = self.db.child("user").get()
+        target_value=[]
+        for res in users.each():
+            value = res.val()
+            if value['id'] == id_ and value['pw'] == pw_:
+                return True
+        return False
+    # 상품등록
     def insert_item(self, name, data, img_path):
         item_info ={
             "seller": data['seller'],
@@ -23,28 +60,18 @@ class DBhandler:
         print(data,img_path)
         return True
     
-    def insert_user(self,data,pw):
-        user_info={
-            "id": data['id'],
-            "pw":pw,
-            "nickname":data['nickname']
-        }
-        if self.user_duplicate_check(str(data['id'])):
-            self.db.child("user").push(user_info)
-            print(data)
-            return True
-        else:
-            return False
-    def user_duplicate_check(self, id_string):
-        users = self.db.child("user").get()
-        
-        print("users###",users.val())
-        if str(users.val()) == "None": # first registration
-            return True
-        else:
-            for res in users.each():
-                value = res.val()
-                if value['id'] == id_string:
-                    return False
-            return True
-        
+    #item 노드 아래 값 가져오기
+    def get_items(self ):
+        items = self.db.child("item").get().val()
+        return items
+    
+    #상품상세화면
+    def get_item_byname(self, name):
+        items = self.db.child("item").get()
+        target_value=""
+        print("###########",name)
+        for res in items.each():
+            key_value = res.key()
+            if key_value == name:
+                target_value=res.val()
+        return target_value
